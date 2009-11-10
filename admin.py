02fac85b3d0956436d4b0e3cfa5c0b7b631a7a48
Skylar Saveland <skylar.saveland@gmail.com>
2009-11-10 00:23:35 -0500

    admin flip-flopping

--- admin.py
+++ admin.py
@@ -3,21 +3,20 @@ from olwidget.admin import GeoModelAdmin
 from geoms.models import Geom, GeomRelation
 
 # we could use the default map
-admin.site.register(Geom, GeoModelAdmin)
+#admin.site.register(Geom, GeoModelAdmin)
 
-'''
 # Customize the map
 class GeomGeoAdmin(GeoModelAdmin):
     options = {
-        #'layers': ['google.satellite', 'google.hybrid',  'google.streets'], #'google.terrain',],#
+        'layers': ['google.satellite', 'google.hybrid',  'google.streets' ], # 'google.terrain', ],
         'default_lat': 44,
         'default_lon': -72,
+        'default_zoom': 1,
     }
     list_display = [ '__unicode__', 'countries', ]
     prepopulated_fields = { 'slug': ('title',) }
 
 admin.site.register(Geom, GeomGeoAdmin)
-'''
 
 admin.site.register(GeomRelation)
 
